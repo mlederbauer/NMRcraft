01f346518b84d26e364fd2cdf1b0a4be41de6d05
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-03-26 22:22:23 +0100

    feat: seed fctn

---
+++ nmrcraft/utils/set_seed.py
@@ -0,0 +1,18 @@
+import random
+
+import numpy as np
+
+# import torch
+
+
+def set_seed(seed: int = 42):
+    """Set the seed for reproducibility."""
+
+    random.seed(seed)
+    np.random.seed(seed)
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed_all(seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
+
+    print(f"Seed set to {seed}.")
