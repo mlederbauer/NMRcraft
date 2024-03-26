32ab33cd493ab99fe0122945ff811417df43c914
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-03-26 22:33:10 +0100

    feat: add title

--- nmrcraft/analysis/plotting.py
+++ nmrcraft/analysis/plotting.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.style.use("./style.mplstyle")
 plt.rcParams["text.latex.preamble"] = r"\usepackage{sansmathfonts}"
 
 
-def plot_predicted_vs_ground_truth(y_test, y_pred):
+def plot_predicted_vs_ground_truth(y_test: np.array, y_pred: np.array, title: str):
     """
     Plots the predicted values against the ground truth values.
 
@@ -22,5 +23,5 @@ def plot_predicted_vs_ground_truth(y_test, y_pred):
     plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "k--", lw=2)
     plt.xlabel("Actual")
     plt.ylabel("Predicted")
-    plt.title("Predicted vs Actual Values")
+    plt.title(title)
     plt.show()
