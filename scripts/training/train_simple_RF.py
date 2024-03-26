0259547e5f463517d4adaa0a6273be70d2201808
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-03-26 22:24:32 +0100

    chore: clean up comments

--- scripts/training/train_simple_RF.py
+++ scripts/training/train_simple_RF.py
@@ -13,22 +13,30 @@ def main():
     """Train a simple random forest model."""
     # Load the data
     dataset = load_data()
-    # only take 1% of the dataset
+
+    # Only take 1% of the dataset
     dataset = dataset.sample(frac=0.01)
 
-    # X is the column "E_sigmaiso_ppm" and "M_sigmaiso_ppm", y is the column "bond_length_ME"
+    # Extract features and target variables
     X = dataset[["E_sigmaiso_ppm", "M_sigmaiso_ppm"]].to_numpy()
     y = dataset["bond_length_M_E"].to_numpy()
 
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = split_data(X, y)
 
+    # Create a pipeline with data preprocessing and random forest regressor
     pipe = make_pipeline(StandardScaler(), RandomForestRegressor())
+
+    # Fit the pipeline to the training data
     pipe.fit(X_train, y_train)
+
+    # Evaluate the model on the testing data
     score = pipe.score(X_test, y_test)
 
+    # Print the R^2 score
     print(f"R^2 score: {score}")
 
-    # Plot the random numbers
+    # Plot the predicted vs ground truth values
     plot_predicted_vs_ground_truth(y_test, pipe.predict(X_test))
 
 
