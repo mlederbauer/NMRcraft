e801ef63fe7e85e1edc98d6f0299fb77f4e8ac49
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-03-26 22:21:02 +0100

    chore: docstrings

--- nmrcraft/analysis/plotting.py
+++ nmrcraft/analysis/plotting.py
@@ -5,6 +5,17 @@ plt.rcParams["text.latex.preamble"] = r"\usepackage{sansmathfonts}"
 
 
 def plot_predicted_vs_ground_truth(y_test, y_pred):
+    """
+    Plots the predicted values against the ground truth values.
+
+    Parameters:
+    - y_test (array-like): The ground truth values.
+    - y_pred (array-like): The predicted values.
+
+    Returns:
+    None
+    """
+
     # Creating the plot
     plt.figure(figsize=(10, 8))
     plt.scatter(y_test, y_pred, color="r", edgecolor="k", alpha=0.6)
