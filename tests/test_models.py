1b097644a67fa8a268c3745ea722240c6a125a14
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-04-23 12:54:38 +0000

    feat: first tests

---
+++ tests/test_models.py
@@ -0,0 +1,27 @@
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+from nmrcraft.models.models import load_model
+
+
+def test_load_model():
+    models = {
+        "random_forest": RandomForestClassifier,
+        "gradient_boosting": GradientBoostingClassifier,
+        "logistic_regression": LogisticRegression,
+        "svc": SVC,
+    }
+
+    for model_name, model_class in models.items():
+        model = load_model(model_name)
+        assert isinstance(model, model_class), f"Expected {model_class}, got {type(model)}"
+
+
+# def test_load_model_unsupported_model():
+#     with pytest.raises(ValueError):
+#         load_model("unsupported_model")
+
+# def test_load_model_unsupported_kwargs():
+#     with pytest.raises(TypeError):
+#         load_model("random_forest", "rainbows")
