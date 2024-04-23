84a4b3ceff03ae368b0b2cf3e259f315a2486a7d
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-04-23 13:40:14 +0000

    feat: add tests for new load_model fctn

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,4 @@
+import pytest
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -18,10 +19,11 @@ def test_load_model():
         assert isinstance(model, model_class), f"Expected {model_class}, got {type(model)}"
 
 
-# def test_load_model_unsupported_model():
-#     with pytest.raises(ValueError):
-#         load_model("unsupported_model")
+def test_load_model_unsupported_model():
+    with pytest.raises(ValueError):
+        load_model("unsupported_model")
 
-# def test_load_model_unsupported_kwargs():
-#     with pytest.raises(TypeError):
-#         load_model("random_forest", "rainbows")
+
+def test_load_model_unsupported_kwargs():
+    with pytest.raises(TypeError):
+        load_model("random_forest", "rainbows")
