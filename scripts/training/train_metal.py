bf28bdd9b6f374ad79316f8765d9088044ba7a2e
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-04-23 16:51:38 +0000

    feat:" revised training scri[t with hyperparam optimization

--- scripts/training/train_metal.py
+++ scripts/training/train_metal.py
@@ -2,27 +2,28 @@ import argparse
 import os
 
 import mlflow
-from sklearn.metrics import accuracy_score, auc, confusion_matrix, f1_score, roc_curve
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 from nmrcraft.analysis.plotting import plot_confusion_matrix, plot_roc_curve
-
-# Assuming these are your custom modules
 from nmrcraft.data.dataset import load_data
-from nmrcraft.models.hyperparameter_tune import hyperparameter_tune
+from nmrcraft.evaluation.evaluation import model_evaluation
+from nmrcraft.models.model_configs import model_configs
 from nmrcraft.models.models import load_model
+from nmrcraft.training.hyperparameter_tune import HyperparameterTuner
 from nmrcraft.utils.set_seed import set_seed
 
 set_seed()
 
 
 def main(dataset_size, target, model_name):
-    """Train a model with MLflow tracking."""
-    mlflow.set_experiment("Model_Classification_Experiment")
+    # TODO: better experiment naming
+    mlflow.set_experiment("Ceci_nest_pas_un_experiment")
 
     with mlflow.start_run():
-        # Load and preprocess data
+        config = model_configs[model_name]
+
+        # TODO: add data loader in nmrcraft/data/dataset.py
         dataset = load_data()
         dataset = dataset.sample(frac=dataset_size)
 
@@ -44,22 +45,20 @@ def main(dataset_size, target, model_name):
         y = label_encoder.transform(y_label)
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-        model = load_model(model_name, n_estimators=100, random_state=42)
-        model = hyperparameter_tune(model, X_train, y_train)
-        model.fit(X_train, y_train)
 
-        y_pred = model.predict(X_test)
-        score = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred, average="binary")
-        cm = confusion_matrix(y_test, y_pred)
-        fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:, 1])
-        roc_auc = auc(fpr, tpr)
+        tuner = HyperparameterTuner(model_name, config)
+        best_params, _ = tuner.tune(X_train, y_train, X_test, y_test)
 
+        model_func = lambda **params: load_model(model_name, **{**params, **config["model_params"]})
+        best_model = model_func(**best_params)
+        best_model.fit(X_train, y_train)
+
+        metrics, cm, fpr, tpr = model_evaluation(best_model, X_test, y_test)
+        mlflow.log_params(best_params)
         mlflow.log_params({"model_name": model_name, "dataset_size": dataset_size, "target": target})
-        mlflow.log_metrics({"accuracy": score, "f1_score": f1, "roc_auc": roc_auc})
+        mlflow.log_metrics(metrics)
 
-        mlflow.sklearn.log_model(model, "model")
-
+        # TODO: refactor this to a function nmrcraft/analysis/ or nmrcraft/evaluation
         fig_path = "scratch/"
         if not os.path.exists(fig_path):
             os.makedirs(fig_path)
@@ -68,15 +67,14 @@ def main(dataset_size, target, model_name):
         plot_confusion_matrix(cm, classes=label_encoder.classes_, title=title, path=cm_path)
         roc_path = os.path.join(fig_path, "roc.png")
         title = r"ROC curve, TODO add LaTeX symbols"
-        plot_roc_curve(fpr, tpr, roc_auc, title=title, path=roc_path)
+        plot_roc_curve(fpr, tpr, metrics["roc_auc"], title=title, path=roc_path)
 
         mlflow.log_artifact(cm_path)
         mlflow.log_artifact(roc_path)
 
-        # os.remove(cm_path)
-        # os.remove(roc_path)
+        mlflow.sklearn.log_model(best_model, "model")
 
-        print(f"Accuracy: {score}")
+        print(f"Accuracy: {metrics['accuracy']}")
 
 
 if __name__ == "__main__":
