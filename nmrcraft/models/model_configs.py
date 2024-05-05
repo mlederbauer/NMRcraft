ea2edaa5e03dd752a33b82847949b3a54719540a
kbiniek <karo.biniek@gmail.com>
2024-05-05 09:39:00 +0000

    feat: add logistic_regression and svc

--- nmrcraft/models/model_configs.py
+++ nmrcraft/models/model_configs.py
@@ -27,4 +27,36 @@ model_configs = {
             "max_features": hp.choice("max_features", ["sqrt", "log2", None]),
         },
     },
+
+    "logistic_regression": {
+        "model_params": {"random_state": 42},
+        "hyperparameters": {
+            "penalty": hp.choice("penalty", ["l1", "l2", "elasticnet", None]),
+            "C": hp.uniform("C", 0.01, 10.0),
+            "solver": hp.choice("solver", ["saga"]),
+            # lbfgs --> l2, None
+            # liblinear --> l1, l2
+            # newton-cg --> l2, None
+            # newton-cholesky --> l2, None
+            # sag --> l2, None
+            # saga --> l1, l2, elasticnet, None
+            "max_iter": hp.choice("max_iter", range(100, 1000, 100)),
+            "l1_ratio": hp.uniform("l1_ratio", 0.01, 1.0),
+        },
+    },
+
+    "svc": {
+        "model_params": {"random_state": 42},
+        "hyperparameters": {
+            "C": hp.uniform("C", 0.01, 10.0),
+            "kernel": hp.choice("kernel", ["linear", "poly", "rbf", "sigmoid"]),
+            "degree": hp.choice("degree", range(1, 10)),
+            "gamma": hp.choice("gamma", ["scale", "auto"]),
+            "coef0": hp.uniform("coef0", 0.0, 1.0),
+            "shrinking": hp.choice("shrinking", [True, False]),
+            "probability": hp.choice("probability", [True, False]),
+            # "max_iter": hp.choice("max_iter", range(100, 1000, 100)),
+
+        },
+    },
 }
