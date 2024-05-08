e566705d26d78b1298ce3c36e8a0d6b2135400ac
Samuel Stricker <strsamue@ethz.ch>
2024-05-08 21:34:43 +0000

    Added a Visualizer class for easier pipline programming.

---
+++ nmrcraft/evaluation/visualizer.py
@@ -0,0 +1,20 @@
+# Import Libraries
+
+
+class Visulizer:
+    def __init__(
+        self,
+        model_name: str,
+        data: None,
+    ):
+        self.model_name = model_name
+        self.data = data
+
+    def plot_ROC():
+        pass
+
+    def plot_F1():
+        pass
+
+    def plot_Accuracy():
+        pass
