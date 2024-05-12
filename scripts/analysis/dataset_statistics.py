05f2e6def6469d200f5328af128ac3bbd0d461f2
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-05-12 20:31:08 +0000

    feat: core structure of analyhsis scripts

---
+++ scripts/analysis/dataset_statistics.py
@@ -0,0 +1,17 @@
+"""Script to show important statist8ics of the dataset, such as ligand classes, ligand class distriutions."""
+
+# import style for matplotlib
+
+# load dataset
+
+# plottinf functions
+
+# save plots
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/analysis/pca_ligand_space.py
@@ -0,0 +1,15 @@
+"""Script to plot a PCA of the complexes according to their principal components."""
+
+# load data
+
+# color the plots according to the complex class (metals, e-ligand, x-ligands)
+
+# main script to exec ute this
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
