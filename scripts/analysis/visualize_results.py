f306162143321911d127ca83e4141ec19fe080c8
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-05-29 09:20:12 +0000

    make modular. attempt to fix.

--- scripts/analysis/visualize_results.py
+++ scripts/analysis/visualize_results.py
@@ -5,24 +5,32 @@ import pandas as pd
 
 from nmrcraft.analysis.plotting import plot_bar, plot_metric
 
-if __name__ == "main":
 
+def load_results(results_dir: str, baselines_dir: str):
     import_filename_base = "metrics/results_baselines.csv"
-    import_filename_one = "metrics/results_one_target.csv"
-    import_filename_multi = "metrics/results_multi_target.csv"
+    import_filename_one = "metrics/20eval/results_one_target.csv"
+    import_filename_multi = "metrics/20eval/results_multi_target.csv"
 
     df_base = pd.read_csv(import_filename_base)
     df_one = pd.read_csv(import_filename_one)
     df_multi = pd.read_csv(import_filename_multi)
 
-    df = pd.concat([df_base, df_one])
+    return df_base, df_one, df_multi
 
-    if not os.path.exists("./plots"):
-        os.makedirs("./plots")
 
-    targets = df["target"].unique()
+def plot_exp_1(df_base, df_one):
+    """Compare single output models with baselines for accuracy/f1-score as a function of dataset size."""
+    # x axis = datase size
+    # y axis = accuracy or f1 score
+    # legend on the bottom of the plot (below the plot itself)
+    # color according to the used model
+    # use bar plots for one dataset_size and put them next to each other
+
+    df_combined = pd.concat([df_base, df_one])
+
+    targets = df_combined["target"].unique()
     for target in targets:
-        sub_df = df[df["target"] == target]
+        sub_df = df_combined[df_combined["target"] == target]
         print(sub_df)
         plot_metric(
             sub_df,
@@ -40,9 +48,19 @@ if __name__ == "main":
             iterative_column="model",
             xdata="dataset_fraction",
         )
+    return
 
-    df = pd.concat([df_one, df_multi])
-    full_df = df[df["dataset_fraction"] == 1]
+
+def plot_exp_2(df_one, df_multi):
+    """Compare a single-output model to its multi-output model counterpart. For the targets
+    x axis = metal, metal E, metal X3, E X3, metal E X3
+    y axus = accuracy or f1 score
+    use the best one-target/multi-target model for the target to plot
+    and take dataset_size = 1.0
+    use bar plot
+    """
+    df_combined = pd.concat([df_one, df_multi])
+    full_df = df_combined[df_combined["dataset_fraction"] == 1]
 
     true_df = full_df[full_df["nmr_only"]]
     models = true_df["model"].unique()
@@ -79,8 +97,17 @@ if __name__ == "main":
         ),
         axis=1,
     )
+    return
 
-    print(full_df)
+
+def plot_exp_3(df_one, df_multi):
+    """Compare whether nmr-only is set to true or false
+    plot for X3 (best one target model) the bar plot with/without ligands
+    plot for metal & E & X3 (best multi target model) the bar plot with/withut ligands
+    legens also below the plot itself
+    """
+    df_combined = pd.concat([df_one, df_multi])
+    full_df = df_combined[df_combined["dataset_fraction"] == 1]
 
     models = full_df["model"].unique()
     for model in models:
@@ -102,3 +129,18 @@ if __name__ == "main":
             iterative_column="target",
             xdata="xlabel",
         )
+    return
+
+
+if __name__ == "__main__":
+
+    if not os.path.exists("./plots/results/"):
+        os.makedirs("./plots/results/")
+
+    df_base, df_one, df_multi = load_results(
+        results_dir="metrics/20eval/", baselines_dir="metrics/"
+    )
+
+    plot_exp_1(df_base, df_one)
+    plot_exp_2(df_one, df_multi)
+    plot_exp_3(df_one, df_multi)
