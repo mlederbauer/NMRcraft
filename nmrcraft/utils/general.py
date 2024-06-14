ade49ba15ab6c619456e1ecb8d254b40f567862f
Tiago Würthner <twuerthner@ehtz.ch>
2024-06-14 11:02:50 +0000

    Clarify general.py

--- nmrcraft/utils/general.py
+++ nmrcraft/utils/general.py
@@ -13,30 +13,43 @@ def add_rows_metrics(
     Compiles and adds a series of statistical metrics into a unified DataFrame, one row at a time.
 
     Args:
-        statistical_metrics (list): List of lists containing metrics that match by their respective indices.
+        statistical_metrics (list): List of lists containing the mean and confidence intervals of
+        accuracy and F1-score.
         dataset_size (int): Number of samples in the dataset.
         include_structural (bool): Indicates whether structural data was included in the analysis.
-        model_name (str):  Identifier for the model that produced the metrics.
-        max_evals (int): Number of evaluations conducted.
+        model_name (str):  Name of the model that produced the metrics.
+        max_evals (int): Number of evaluations conducted in the Hyperparameter tuning.
 
     Returns:
-        unified_metrics (pd.DataFrame): DataFrame to append new statistical metrics.
-    """
+        unified_metrics (pd.DataFrame): DataFrame with all metrics containing these columns:
+            target, model_targets, model, nmr_only, dataset_fraction, max_evals, accuracy_mean,
+            accuracy_lb, accuracy_hb, f1_mean, f1_lb, f1_hb
 
+    """
+    # Give meaning to indices
+    idx_name = 0
+    idx_accuracy_mean = 1
+    idx_accuracy_ci = 2
+    idx_f1score_mean = 3
+    idx_f1score_ci = 4
+    idx_lb = 0
+    idx_hb = 1
+
+    # Combine all data into single row and append to dataframe
     for i in range(len(statistical_metrics[0])):
         new_row = [
-            statistical_metrics[0][i],
-            statistical_metrics[0],
+            statistical_metrics[idx_name][i],
+            statistical_metrics[idx_name],
             model_name,
             not include_structural,
             dataset_size,
             max_evals,
-            statistical_metrics[1][i],
-            statistical_metrics[2][i][0],
-            statistical_metrics[2][i][1],
-            statistical_metrics[3][i],
-            statistical_metrics[4][i][0],
-            statistical_metrics[4][i][1],
+            statistical_metrics[idx_accuracy_mean][i],
+            statistical_metrics[idx_accuracy_ci][i][idx_lb],
+            statistical_metrics[idx_accuracy_ci][i][idx_hb],
+            statistical_metrics[idx_f1score_mean][i],
+            statistical_metrics[idx_f1score_ci][i][idx_lb],
+            statistical_metrics[idx_f1score_ci][i][idx_hb],
         ]
         unified_metrics.loc[len(unified_metrics)] = new_row
     return unified_metrics
