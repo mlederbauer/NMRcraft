d1c892e55b584796006343cbcf4d6e4ac17007ff
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-06-21 22:03:58 +0000

    feat: refactor, fix paths

--- scripts/training/one_target.py
+++ scripts/training/one_target.py
@@ -1,3 +1,5 @@
+"""Script to train and evaluate models with one target."""
+
 import argparse
 import logging as log
 import os
@@ -6,13 +8,16 @@ import mlflow
 import numpy as np
 import pandas as pd
 
-from nmrcraft.analysis import plotting
-from nmrcraft.data.dataloader import DataLoader
-from nmrcraft.evaluation import evaluation
-from nmrcraft.models.model_configs import model_configs
-from nmrcraft.models.models import load_model
-from nmrcraft.training.hyperparameter_tune import HyperparameterTuner
-from nmrcraft.utils.general import add_rows_metrics, str2bool
+from nmrcraft.analysis import plot_confusion_matrix
+from nmrcraft.data import DataLoader
+from nmrcraft.evaluation import (
+    evaluate_bootstrap,
+    evaluate_model,
+    metrics_statistics,
+)
+from nmrcraft.models import load_model, model_configs
+from nmrcraft.training import HyperparameterTuner
+from nmrcraft.utils import add_rows_metrics, str2bool
 
 # Setup MLflow
 mlflow.set_experiment("Final_Results")
@@ -123,11 +128,9 @@ def main(args) -> pd.DataFrame:
                 best_model.fit(X_train, np.squeeze(y_train))
                 y_pred = np.atleast_2d(best_model.predict(X_test)).T
 
-                metrics, cm_list = evaluation.evaluate_model(
-                    y_test, y_pred, args.target
-                )
+                metrics, cm_list = evaluate_model(y_test, y_pred, args.target)
 
-                plotting.plot_confusion_matrix(
+                plot_confusion_matrix(
                     cm_list,
                     y_labels,
                     model_name,
@@ -135,13 +138,11 @@ def main(args) -> pd.DataFrame:
                     args.plot_folder,
                 )
 
-                bootstrap_metrics = evaluation.evaluate_bootstrap(
+                bootstrap_metrics = evaluate_bootstrap(
                     X_test, y_test, best_model, args.target
                 )
 
-                statistical_metrics = evaluation.metrics_statistics(
-                    bootstrap_metrics
-                )
+                statistical_metrics = metrics_statistics(bootstrap_metrics)
 
                 # Add all the newly generated metrics to the unified dataframe
                 unified_metrics = add_rows_metrics(
